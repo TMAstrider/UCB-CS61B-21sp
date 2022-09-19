0c6302484d441e1f05038c5286aa482517d5c11a
TMAstrider <t.m.a.strider@gmail.com>
2022-09-19 11:38:47 +0800

    implemented NextNumber Func

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,23 +3,23 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** TMAstrider's implementation of NextNumber
+     * @source TMAstrider
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
     public static void main(String[] args) {
-        int n = 5;
+        int n = 1567899;
         System.out.print(n + " ");
         while (n != 1) {
             n = nextNumber(n);
-            System.out.print(n + " ");
+            System.out.println(n + " ");
         }
         System.out.println();
     }
