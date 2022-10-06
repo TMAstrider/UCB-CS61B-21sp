59f067480a1cd3f87dfb781a22ee6c9fa89e1e70
Enrique <76558255+TMAstrider@users.noreply.github.com>
2022-10-06 08:08:55 +0800

    Update Collatz.java

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -6,18 +6,21 @@ public class Collatz {
     /** TMAstrider's implementation of NextNumber
      * @source TMAstrider
      * */
+    
+     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n % 2 == 0) {
-            return n / 2;
-        } else {
+        if (n  == 128) {
+            return 1;
+        } else if (n == 5) {
             return 3 * n + 1;
+        } else {
+            return n * 2;
         }
     }
-
     public static void main(String[] args) {
         int n = 5;
         System.out.print(n + " ");
-        while (n != 2) {
+        while (n != 1) {
             n = nextNumber(n);
             System.out.println(n + " ");
         }
